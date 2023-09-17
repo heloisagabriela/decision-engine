3dc26411d4be45fec47215b904b1e5dec80595db
Heloisa Gabriela <heloisagabrielavieira@gmail.com>
2023-09-17 02:40:40 -0300

    feat: creation of unit tests

---
+++ src/sourceTests.py
@@ -0,0 +1,39 @@
+import unittest
+from source import getPolicyDB, parameterValidation, result
+
+# Import other functions and classes from your module as needed
+
+class TestPolicyFunctions(unittest.TestCase):
+
+    def test_getPolicyDB(self):
+        # Create a temporary CSV file with sample data for testing
+        with open("policyDB.csv", mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['variable', 'operator', 'value'])
+            writer.writerow(['age', '>', '18'])
+            writer.writerow(['incoming', '>=', '1000'])
+
+        # Test whether getPolicyDB reads the CSV file and returns a NumPy array
+        policy_db = getPolicyDB()
+        self.assertIsNotNone(policy_db)
+        self.assertEqual(len(policy_db), 2)
+
+    def test_parameterValidation(self):
+        # Test various parameter validation scenarios
+        self.assertTrue(parameterValidation(20, ['age', '>', '18']))
+        self.assertFalse(parameterValidation(15, ['age', '>', '18']))
+        self.assertTrue(parameterValidation(1000, ['incoming', '>=', '1000']))
+        self.assertFalse(parameterValidation(900, ['incoming', '>=', '1000']))
+        self.assertTrue(parameterValidation(1000, ['incoming', '==', '1000']))
+
+    def test_result(self):
+        # Test the result function
+        self.assertEqual(result(True, True), {'decision': True})
+        self.assertEqual(result(True, False), {'decision': False})
+        self.assertEqual(result(False, True), {'decision': False})
+        self.assertEqual(result(False, False), {'decision': False})
+
+    # You can similarly write tests for other functions
+
+if __name__ == '__main__':
+    unittest.main()
